36012efb8071f93739a7931ed4b1571ab5ec9741
Paula Vilau <vilaupaula@yahoo.ro>
2023-10-19 19:30:27 +0300

    Adaugare controller MyFirstController

---
+++ Controllers/MyFirstController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vilau_Paula_Lab_1.Controllers
+{
+    public class MyFirstController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome";
+        }
+
+        public string Hello()
+        {
+            return "Hello";
+        }
+
+        public string RandomMethod(string mesaj, int numar)
+        {
+            return $"Mesaj: {mesaj}, Numar: {numar}";
+        }
+    }
+}
